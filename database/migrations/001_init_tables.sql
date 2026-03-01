9aafbba35d2f272c51add1cf77a2be3f29636a6c
shakhawat-hossain-shoaib <shakhawat.cse.20230104081@aust.edu>
2026-03-01 06:48:00 +0600

    login added

--- database/migrations/001_init_tables.sql
+++ database/migrations/001_init_tables.sql
@@ -1,22 +1,11 @@
-CREATE DATABASE IF NOT EXISTS `attendance_tracker` DEFAULT CHARACTER SET latin1 COLLATE latin1_danish_ci;
+CREATE DATABASE IF NOT EXISTS `flatease` DEFAULT CHARACTER SET latin1 COLLATE latin1_danish_ci;
 
-DROP TABLE IF EXISTS `attendances`;
-DROP TABLE IF EXISTS `sessions`;
-
-CREATE TABLE sessions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    duration INT NOT NULL,
-    active BOOLEAN DEFAULT TRUE
-);
-
-CREATE TABLE `attendances` (
-    `id` INT  AUTO_INCREMENT PRIMARY KEY,
-    `session_id` INT  NOT NULL,
-    `roll` VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (`session_id`) REFERENCES `sessions`(`id`)
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'tenant') DEFAULT 'tenant',
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL
 );
